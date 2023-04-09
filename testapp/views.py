b283816b3066e3f3b7da597206cec868c33bd2ba
oumaimaezafa <oumaimaezafa12@gmail.com>
2023-04-09 06:12:42 +0100

    second commit

--- testapp/views.py
+++ testapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
+from .form import SignUpForm
 # Create your views here.
 def hell_world(request):
      #ceck to see if loggin in 
@@ -29,6 +30,21 @@ def logout_user(request):
     return redirect('home')
 
 def register_user(request):
-     return render(request,'register.html',{})
+    if request.method=='POST':
+      form=SignUpForm(request.POST)
+      if form.is_valid():
+         form.save()
+         #login
+         username=form.cleaned_data['username']
+         password=form.cleaned_data['password1']
+         user =authenticate(username=username,password=password)
+         login(request,user)
+         messages.success(request,"You have successed !!! ")
+         return redirect('home')
+    else:
+        form=SignUpForm()
+        return render(request,'register.html',{'form':form})
+
+    return render(request,'register.html',{'form':form})
 
 
